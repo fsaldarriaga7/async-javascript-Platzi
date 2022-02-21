4e2e7d7381fc7a6f70c48f01cc5de27a0719a002
Felipe Saldarriaga <saldarriagafelipe3@gmail.com>
2022-02-21 15:25:41 -0500

    Creados los archivos de challenge, para comenzar con la integración de APIs

--- src/callback/index.js
+++ src/callback/index.js
@@ -8,4 +8,19 @@ function calc(num1, num2, callback) {
 
 console.log( calc (6 ,2 ,sum) );
 
+function date (callback) {
+    console.log(new Date);
+    setTimeout(function() {
+        let date = new Date;
+        callback (date);
+    }, 3000 )
+}
+
+function printDate(dateNow) {
+    console.log(dateNow)
+}
+
+date(printDate);
+
+//Ejecutar npm run callback en la Terminal
 

f58261f5a2307549570b59a595ac25363866d897
Felipe Saldarriaga <saldarriagafelipe3@gmail.com>
2022-02-21 15:48:00 -0500

    Agregados todos los archivos nuevos, para trabajar en el challenge

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,1 +1,19 @@
-//En consola, 
+//En consola, npm install xmlhttprequest --save
+
+let XMLHttpRequest = requiere('xmlhttprequest').XMLHttpRequest;
+
+function fetchData (url_api, callback){
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = function (event) {
+        if(xhttp.readyState === 4) {
+            if(xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error('Error' + url_api);
+                return callback (error, null);
+            }
+        }
+    }
+    xhttp.send();
+}
